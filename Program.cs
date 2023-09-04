8e243c9bd798910d32df20a2c127e83758505154
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-04 20:54:44 +0530

    Encapsulation

--- Program.cs
+++ Program.cs
@@ -2,25 +2,13 @@
 {
     internal class Program
     {
-        int a = 10;
-        int b = 20;
-        int c = 0;
-        void add()
-        {
-            c = a + b;
-            Console.WriteLine("addition of " + a + " and " + b + " is: " + c);
-        }
-        void sub()
-        {
-            c=a - b;
-            Console.WriteLine("substraction  of " + a + " and " + b + " is: " + c);
-        }
         static void Main(string[] args)
         {
-            Program Oops=new Program();
-            
-            Oops.add();
-            Oops.sub();
+            Encapsulation encapsulation = new Encapsulation();
+            encapsulation.Name = "Aditya";
+            encapsulation.salary = 25000;
+            Console.WriteLine("Employee Name: "+encapsulation.Name);
+            Console.WriteLine("Salary is :" + encapsulation.salary);
         }
     }
 }
