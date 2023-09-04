c4643f8002bc27937b322d9c957b599de8521c2f
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-04 20:21:31 +0530

    [aditya]: Created class and object with example

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Oops_Concept
+{
+    internal class Program
+    {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        void add()
+        {
+            c = a + b;
+            Console.WriteLine("addition of " + a + " and " + b + " is: " + c);
+        }
+        void sub()
+        {
+            c=a - b;
+            Console.WriteLine("substraction  of " + a + " and " + b + " is: " + c);
+        }
+        static void Main(string[] args)
+        {
+            Program Oops=new Program();
+            
+            Oops.add();
+            Oops.sub();
+        }
+    }
+}
