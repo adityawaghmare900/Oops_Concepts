d92e92ff508cb77fd2f9921cbf7cd9ac9b6bb2f0
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-06 15:38:48 +0530

    [aditya]: defulat and perameterized constructors

---
+++ Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Car
+    {
+        public String model;
+
+        //defualt Constructor
+        public Car()
+        {
+            model = "Fortuner";
+        }
+    }
+
+    internal class Bike
+    {
+        public String model;
+        public Bike(String bikeName)
+        {
+            model = bikeName;
+        }
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,38 +1,17 @@
-﻿using System.Diagnostics;
+﻿using Constructor;
+using System.Diagnostics;
 
-namespace TypesOfVaribales
+namespace Vonstructor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //There are five types of varibales
-            //1) int - it stores intergers number, withour decimals 
-            int num = 25;
-            Console.WriteLine(num);
+           Car car = new Car();
+            Console.WriteLine(car.model);
 
-            //2) double- it stores floating point numbers with decimals
-
-            double dob = 199.54D;
-            Console.WriteLine(dob);
-
-            //3) char- it stores single charactor with single quotes
-            char ch = 'A';
-            char ch2 = 'B';
-            Console.WriteLine(ch);
-            Console.WriteLine(ch2);
-
-            //4) String - it stores sequence of charactors or it stores text
-            String str = "aditya";
-            String str2 = "waghmare";
-            Console.WriteLine(str);
-            Console.WriteLine(str2);
-
-            //5) bool- it store with two states: true of false
-            bool a = true;
-            bool b = false;
-            Console.WriteLine(a);
-            Console.WriteLine(b);
+            Bike bike = new Bike("Hayabhusa");
+            Console.WriteLine(bike.model);
         }
     }
 }
